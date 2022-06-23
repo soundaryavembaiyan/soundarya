7a4fbb976e7669b540d6b74adff5a73f00692a42
Soundarya Vembaiyan <53108412+soundaryavembaiyan@users.noreply.github.com>
2022-06-23 03:48:42 -0700

    Add files via upload

---
+++ Angular/ngFor/app.component.ts
@@ -0,0 +1,52 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ngFor';
+  friendslist = [
+    {
+      name: 'Nishant',
+      age: 25
+    },
+    {
+      name: 'Shailesh',
+      age: 45
+    },
+    {
+      name: 'Abhishek',
+      age: 36
+    },
+    {
+      name: 'Akshay',
+      age: 65
+    },
+    {
+      name: 'Ashish',
+      age: 12
+    },
+    {
+      name: 'Uday',
+      age: 31
+    },
+    {
+      name: 'Mayank',
+      age: 45
+    },
+    {
+      name: 'Raju',
+      age: 74
+    },
+  ]
+
+isVisible: boolean = true;
+
+hideList(){
+  this.isVisible = false;
+}
+
+}
