154743a2a81551e345de1c9800d44f23d7cc354a
Soundarya Vembaiyan <53108412+soundaryavembaiyan@users.noreply.github.com>
2022-07-02 11:11:56 +0530

    Add files via upload

---
+++ Angular/ProductCart/products.ts
@@ -0,0 +1,30 @@
+export interface Product {
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+  }
+  
+  export const products = [
+    {
+      id: 1,
+      name: 'Phone XL',
+      price: 699,
+      description: 'A large phone with one of the best screens'
+    },
+    {
+      id: 2,
+      name: 'Phone Mini',
+      price: 459,
+      description: 'A great phone with one of the best cameras'
+    },
+    {
+      id: 3,
+      name: 'Phone Standard',
+      price: 799,
+      description: 'A cute phone with one of the best sizes'
+    }
+  ];
+  
+  
+  
